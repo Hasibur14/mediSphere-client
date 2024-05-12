6fe48ae85bc8fa8633e07079b94ba40f4ae2066e
hasibur14 <hasib141415@gmail.com>
2024-05-12 23:55:19 +0600

    added booked service using user email

--- src/pages/Dashboard/BookedService.jsx
+++ src/pages/Dashboard/BookedService.jsx
@@ -1,9 +1,90 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
 
 
 const BookedService = () => {
+
+
+    const { user } = useContext(AuthContext) || {};
+    console.log('booked service from...', user?.displayName)
+
+    const [bookedServices, setBookedServices] = useState([])
+
+    useEffect(() => {
+        if (user?.email) {
+            fetch(`${import.meta.env.VITE_API_URL}/booked/${user?.email}`)
+                .then(response => response.json())
+                .then(data => {
+                    setBookedServices(data)
+                })
+        }
+    }, [user?.email]);
+
+
+
+    const formatDate = (isoDate) => {
+        const date = new Date(isoDate);
+        const options = { year: 'numeric', month: 'short', day: 'numeric' };
+        return date.toLocaleDateString('en-US', options);
+    };
+
+
+
     return (
-        <div>
-            <h2>Booked service</h2>
+        <div className="container mx-auto my-16 border rounded-md">
+            <div className="text-center p-4">
+                <h2 className="text-3xl font-bold ">Booked service</h2>
+            </div>
+            <div className="flex flex-col">
+                <div className="-m-1.5 overflow-x-auto">
+                    <div className="p-1.5 min-w-full inline-block align-middle">
+                        <div className="overflow-hidden">
+                            <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+                                <thead className="bg-purple-500">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3 text-start text-xs font-semibold text-white uppercase dark:text-neutral-500">No</th>
+                                        <th scope="col" className="px-6 py-3 text-start text-xs font-semibold text-white uppercase dark:text-neutral-500">Service</th>
+                                        <th scope="col" className="px-6 py-3 text-start text-xs font-semibold text-white uppercase dark:text-neutral-500">Doctor Name</th>
+                                        <th scope="col" className="px-6 py-3 text-start text-xs font-semibold text-white uppercase dark:text-neutral-500">Email</th>
+                                        <th scope="col" className="px-6 py-3 text-start text-xs font-semibold text-white uppercase dark:text-neutral-500">Price</th>
+                                        <th scope="col" className="px-6 py-3 text-start text-xs font-semibold text-white uppercase dark:text-neutral-500">Date</th>
+                                        <th scope="col" className="px-6 py-3 text-end text-xs font-semibold text-white uppercase dark:text-neutral-500">status</th>
+                                    </tr>
+                                </thead>
+                                {
+                                    bookedServices.map((item, index) => (
+                                        <tbody key={item._id} className="divide-y divide-gray-200 dark:divide-neutral-700">
+                                            <tr>
+                                                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                                                    {index + 1}
+                                                </td>
+                                                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
+                                                    {item.serviceName}
+                                                </td>
+                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
+                                                    {item.providerName}
+                                                </td>
+                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
+                                                    {item.providerEmail}
+                                                </td>
+                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
+                                                    {item.price}
+                                                </td>
+                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
+                                                    {formatDate(item.serviceDate)}
+                                                </td>
+                                                <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                                                    <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400">{item.status}</button>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    ))
+                                }
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Dashboard/ManageService.jsx
+++ src/pages/Dashboard/ManageService.jsx
@@ -65,7 +65,7 @@ const ManageService = () => {
                     <div key={bookService._id} className=" w-full max-w-md overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 border">
                         <img className="object-cover object-center w-full h-56" src={bookService.image} alt="image not found" />
 
-                        <div className="flex justify-center bg-gray-900">
+                        <div className="flex justify-center bg-neutral-200">
                             <img className="w-36" src={title} alt="" />
                         </div>
 
@@ -84,14 +84,14 @@ const ManageService = () => {
                                         <path fillRule="evenodd" clipRule="evenodd" d="M16.2721 10.2721C16.2721 12.4813 14.4813 14.2721 12.2721 14.2721C10.063 14.2721 8.27214 12.4813 8.27214 10.2721C8.27214 8.063 10.063 6.27214 12.2721 6.27214C14.4813 6.27214 16.2721 8.063 16.2721 10.2721ZM14.2721 10.2721C14.2721 11.3767 13.3767 12.2721 12.2721 12.2721C11.1676 12.2721 10.2721 11.3767 10.2721 10.2721C10.2721 9.16757 11.1676 8.27214 12.2721 8.27214C13.3767 8.27214 14.2721 9.16757 14.2721 10.2721Z" /><path fillRule="evenodd" clipRule="evenodd" d="M5.79417 16.5183C2.19424 13.0909 2.05438 7.3941 5.48178 3.79418C8.90918 0.194258 14.6059 0.0543983 18.2059 3.48179C21.8058 6.90919 21.9457 12.606 18.5183 16.2059L12.3124 22.7241L5.79417 16.5183ZM17.0698 14.8268L12.243 19.8965L7.17324 15.0698C4.3733 12.404 4.26452 7.9732 6.93028 5.17326C9.59603 2.37332 14.0268 2.26454 16.8268 4.93029C19.6267 7.59604 19.7355 12.0269 17.0698 14.8268Z" />
                                     </svg>
 
-                                    <h1 className="px-2 text-sm">{bookService.serviceArea}</h1>
+                                    <h1 className="px-2 ">{bookService.serviceArea}</h1>
                                 </div>
                             </div>
                             <div
-                                className="md:flex justify-between mt-4">
+                                className="md:flex justify-between mt-4 ">
                                 <Link to={`/serviceUpdate/${bookService._id}`}
-                                    className='flex w-28  text-white  px-4 py-2 rounded-md bg-sky-500'>
-                                    <span className="text-xl mr-2">
+                                    className='flex  text-white  px-6 py-2 rounded-md bg-sky-500'>
+                                    <span className="text-2xl mr-2">
                                         <MdOutlineBrowserUpdated />
                                     </span>
                                     Update
@@ -99,8 +99,8 @@ const ManageService = () => {
 
                                 <button
                                     onClick={() => handleDelete(bookService._id)}
-                                    className='flex w-28 bg-[#EA4744] text-white  px-4 py-2 rounded-md'>
-                                    <span className="text-xl mr-2">
+                                    className='flex bg-[#EA4744] text-white  px-6 py-2 rounded-md'>
+                                    <span className="text-2xl mr-2">
                                         <AiOutlineDelete />
                                     </span>
                                     Delete
